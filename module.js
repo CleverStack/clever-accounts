ee609bec85dfa6ed42aafcd87a3d7e4bedf6c365
Richard Gustin <richard@rgnconsulting.com.au>
2015-06-11 03:48:14 +1000

    chore(cleanup): Remove old preRoute module hook

--- module.js
+++ module.js
@@ -1,19 +1,7 @@
 var Module         = require('classes').Module;
 
 var CleverAccounts = Module.extend({
-  preRoute: function(UserModel, AccountModel, _) {
-    UserModel.on('beforeAllFindersOptions', function(findOptions, queryOptions, callback) {
-      findOptions.include = findOptions.include || [];
-
-      if (!_.findWhere(findOptions.include, { model: AccountModel.entity })) {
-        findOptions.include.push({
-          model : AccountModel.entity
-        });
-      }
-
-      callback(null);
-    });
-  }
+  
 });
 
 module.exports = CleverAccounts;
